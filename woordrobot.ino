595aa0ac93887f1f870a8d812459bfc52d6f6b6c
leroyvandijk <s1104794@windesheim.nl>
2017-03-17 14:23:08 +0100

    dikke update

--- woordrobot.ino
+++ woordrobot.ino
@@ -9,14 +9,16 @@ void setup() {
 }
 
 void loop(){
-  draai(255,false,3);
-  delay(3000);
-  draai(255,true,3);
-  delay(3000);
-  rijd(255, true,3);
-  delay(3000);
-  rijd(255,false,3);
-  delay(3000);
+  //draai(255,false,3);
+  //delay(3000);
+  //draai(255,true,3);
+  //delay(3000);
+  //rijd(255, true,1);
+  //delay(500);
+  //rijd(255,false,1);
+  //delay(3000);
+  schrijfletter('A');
+  delay(100000);
 }
 
 // Functie om motoren te laten draaien, motor moet 1 of 2 zijn, power tot 255, 
@@ -29,17 +31,50 @@ void motor(byte motor, byte power, boolean richting) {
     analogWrite(motorpin[motor-1], power);
 }
 
+void schrijfletter(char L){
+  //tiencmomhoog();
+  switch (L){
+    case 'A':
+      schrijfa();
+      break;
+    case 'M':
+      break;
+    case 'X':
+      break;
+  }
+}
+void tiencmomhoog(){
+  rijd(255,true, 1.2);
+}
+void schrijfa(){
+  //rijd(255,true, 2);
+  draai(200,true, 1);
+  rijd(255,true,2);
+  draai(200,false,1.5);
+  rijd(255,false,2);
+  rijd(255,true,1);
+  draai(200,false,1);
+  rijd(255,true,0.5);
+  draai(200,true,1);
+  rijd(255,false,1);
+  
+}
 // Rijd voor of achteruit voor een bepaalde tijd in seconden
-void rijd(byte power, boolean richting, int duur) {
+void rijd(byte power, boolean richting, float duur) {
  motor(1,power,richting);
- motor(2,power,richting);
+ if(richting){
+ motor(2,power-20,richting);
+ } else{
+ motor(2,power-40,richting);  
+ }
  delay(duur*1000);
  stastil();
 }
 
 // Stop met rijden
 void stastil() {
- rijd(0, false, 0); 
+ motor(1, 0, false); 
+ motor(2, 0, false);
 }
 
 // Draai de robot voor een bepaalde tijd
@@ -48,5 +83,17 @@ void draai(byte power, boolean richting, int duur) {
   motor(2,power,!richting);
   delay(duur*1000);
   stastil();
+}
+
+// Rijd voor of achteruit voor een bepaalde tijd in seconden
+void rijd(byte powerlinks, byte powerrechts, boolean richting, float duur) {
+ motor(1,powerlinks,richting);
+ if(richting){
+ motor(2,powerrechts,richting);
+ } else{
+ motor(2,powerrechts,richting);  
+ }
+ delay(duur*1000);
+ stastil();
 }
 
