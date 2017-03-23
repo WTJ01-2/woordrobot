c8b797da8265a1e49db5f9c7ed3630643a199666
Harmenn <Harmenn@users.noreply.github.com>
2017-03-23 14:32:17 +0100

    Whoehoe het lijkt ergens op

--- woordrobot.ino
+++ woordrobot.ino
@@ -1,6 +1,23 @@
-byte pins [] = {5,4,6,7, 1, 2, 3, 8, 9, 10, 11, 12, 13 };
+byte pins [] = {5,4,6,7 };
 byte motorpin [] = {pins[0],pins[2]};
 byte richtingpin [] = {pins[1],pins[3]};
+int motor_rechts = 1; //1 IS RECHTS
+int motor_links = 2; //2 IS LINKS
+
+char woord[] = {'M', 'A', 'X'};
+
+int sensor1 = analogRead(A0);
+int sensor2 = analogRead(A1);
+int sensor3 = analogRead(A2);
+
+boolean opzoekNaarLijn = false;
+String loopstatus = "start";
+int sensor1licht = 0;
+int sensor2licht = 0;
+int sensor3licht = 0;
+int sensor1donker = 0;
+int sensor2donker = 0;
+int sensor3donker = 0;
 
 boolean rechts = false;
 boolean links = true;
@@ -8,33 +25,177 @@ boolean links = true;
 boolean achteruit = false;
 boolean vooruit = true;
 
-char woord[] = {'M', 'A', 'X'};
-
 boolean start = false;
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   for(byte i = 0; i < sizeof(pins);i++) {
     pinMode(i, OUTPUT);
+    pinMode(1, OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
   }
   
   // pins voor lijnsensoren
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
-  
+
   // pin voor startknop
   pinMode(A3, INPUT);
 }
 
 void loop(){
-  while(!start) {
+//    Serial.println("Sensor 1 min: ");
+//    Serial.println(sensor1licht);
+//    Serial.println("Sensor 1 max: ");
+//    Serial.println(sensor1donker);
+//    
+//    Serial.println("Sensor 2 min: ");
+//    Serial.println(sensor2licht);
+//    Serial.println("Sensor 2 max: ");
+//    Serial.println(sensor2donker);
+//    
+//    Serial.println("Sensor 3 min: ");
+//    Serial.println(sensor3licht);
+//    Serial.println("Sensor 3 max: ");
+//    Serial.println(sensor3donker);
+//  Serial.println("Sensor 2 min: " + sensor2licht+" max: "+sensor2donker);
+//  Serial.println("Sensor 3 min: "+sensor3licht+" max: "+sensor3donker);
+//  
+//  
+    
+    
+    while(!start) {
      start = checkbutton(); 
   }
-  schrijfletter('A');
   
-  // Einde van de loop
-  start = false;
+      if(scanVeld()) return;
+      if(opzoekNaarLijn)
+      {
+        zoekReferentieLijn();
+        return;
+      }
+      rijd(0.8);
+      for(int i = 0; i < sizeof(woord); i++) {
+        char letter = woord[i];
+        Serial.println(letter);
+        Serial.println("Begin nu aan 10cm omhoog");
+
+        tiencmomhoog(i+1);
+        
+        Serial.println("schrijf de letter");
+        schrijfletter(letter);
+        Serial.println("Terug naar referentielijn");
+        zoekReferentieLijn();
+        //rijd(1.5);
+        Serial.println("op referentielijn, rij nog even door en draai 90 graden");
+        rijd(0.8);
+        draai(1.7, links);
+        Serial.println("Rij over referentielijn");
+        rijd(2.5);
+        Serial.println("Letter A? rij extra ver");
+        if(letter=='A') rijd(1.3);
+        Serial.println("Positie voor volgende letter");
+        if(letter!='X') draai(1.7, links);
+      }
+      zoekReferentieLijn();
+      draai(1.7, rechts);
+      zoekReferentieLijn();
+      start=false;
+      delay(32767);
+      delay(32767);
+    //}
+}
+
+
+boolean zoekReferentieLijn()
+{
+  sensor1 = analogRead(A0);
+  sensor2 = analogRead(A1);
+  sensor3 = analogRead(A2);
+  Serial.println("Zoek lijn");
+  if(sensor1 > sensor1donker) Serial.println("Sensor 1 is zwart");
+  if(sensor2 > sensor2donker) Serial.println("Sensor 2 is zwart");
+  if(sensor3 > sensor3donker) Serial.println("Sensor 3 is zwart");
+    int sCount = 0;
+    if(sensor1 > sensor1donker) sCount++;
+    if(sensor2 > sensor2donker) sCount++;
+    if(sensor3 > sensor3donker) sCount++;
+    if (sensor1 < sensor1donker && sensor3 < sensor3donker) {
+        Serial.println("Moet nu rijden");
+        motor(motor_rechts, 220, vooruit);
+        motor(motor_links, 220, vooruit);
+    } else if (sensor1 > sensor1donker && sensor3 < sensor3donker) { 
+        Serial.println("Bocht naar links");
+        motor(motor_rechts, 255, achteruit);
+        motor(motor_links, 220, vooruit);
+    } else if (sensor3 > sensor3donker && sensor1 < sensor1donker) {
+        Serial.println("Bocht naar rechts");
+        motor( motor_links, 255, achteruit);
+        motor(motor_rechts, 220, vooruit);
+    } else if ((sensor3 > sensor3donker && sensor1 > sensor1donker) || sCount >= 2) {
+        Serial.println("You have reached your destination");
+        opzoekNaarLijn = false;
+        stastil();
+        delay(2000);
+        return true;
+    }
+    
+    
+    zoekReferentieLijn();
+}
+
+
+int scanAantal = 0;
+boolean scanningDone = false;
+boolean scanVeld() {
+    if(scanAantal>5)
+    {
+      if(!scanningDone)
+      {
+
+        Serial.println(sensor1donker);
+        Serial.println(sensor2donker);
+        Serial.println(sensor3donker);
+        rijd(3.5, achteruit, 255);
+        opzoekNaarLijn = true;
+        scanningDone = true;
+      }
+      return false;
+    }
+    scanAantal++;
+    
+    motor(motor_rechts, 255, vooruit);
+    motor(motor_links, 255, vooruit);
+    delay(500);
+
+    sensor1 = analogRead(A0);
+    sensor2 = analogRead(A1);
+    sensor3 = analogRead(A2);
+    if(sensor1licht==0)
+    {
+      sensor1licht = analogRead(A0);
+      sensor2licht = analogRead(A1);
+      sensor3licht = analogRead(A2);
+    }
+    if (sensor1 > sensor1donker){
+      sensor1donker = sensor1;
+    }
+    if (sensor2 > sensor2donker){
+      sensor2donker = sensor2;
+    }
+    if (sensor3 > sensor3donker){
+      sensor3donker = sensor3;
+    }
+    return true;
+    
 }
 
 // Functie om motoren te laten draaien, motor moet 1 of 2 zijn, power tot 255, 
@@ -57,65 +218,86 @@ void schrijfletter(char L){
     case 'M':
       schrijfm();
       break;
-    case 'U':
-      schrijfu();
-      break;
     case 'X':
+      schrijfx();
       break;
   }
 }
-void tiencmomhoog(){
-  rijd(1.5);
+void tiencmomhoog(int cijfer){
+  motor(motor_links, 255, vooruit);
+  motor(motor_rechts, 255, vooruit);
+  //int teller=80; deze voor 10cm
+  int teller = 230;
+  for(int i = 0; i < teller; i++)
+  {
+    schrijfcijfer(cijfer);
+    
+  }
+  dimmen();
 }
 
 void schrijfa(){
   //eerst draai
-  draai(1, rechts); // 1
+  draai(0.75, rechts);
   rijd(2);
-  //2e streep A
-  draai(2, links); // -1
+  
+  draai(1.5, links);
   rijd(2, achteruit);
-  //Terug over zelfde lijn voor middelste streep A
-  rijd(0.8);
-  //Draai voor middelste streep A
-  draai(2.5, links); //-2.5
-  //Middelste streep
+  
   rijd(1);
-  draai(2.5, links); //-5
-  rijd(2);
-  draai(1, links); //-6
-  
-  
-  /*rijd(255,false,1);
-  draai(200,false,2);
-  rijd(255,false,2);*/
+  draai(1.1, links); 
+  //Middelste streep
+  rijd(1.5);
+  draai(1.2, links); 
+  rijd(1);
+  draai(0.75, links);
 }
 
+
 void schrijfm(){
-/*  rijd(255,true,2.2);
-  draai(200,true, 1);
-  rijd(255,false,1);
-  draai(200,false,1.8);
-  rijd(255,true,1);
-  draai(200,true,1.5);
-  rijd(255,false,2.2);*/
+  //eerst streep recht omhoog
+  rijd(2);
+  // draai voor streep naar beneden
+  draai(1, links); // -1
+  rijd(1, achteruit);
+  // draai voor streep naar boven
+  draai(1.5, links);
+  // lijn
+  rijd(1, achteruit);
+  // draai voor laatste lijn
+  draai(1, links);
+  // naar beneden rijden en done
+  rijd(2);
 }
-void schrijfu(){/*
-  rijd(255,true,2.2);
-  rijd(255,false,2.2); 
-  draai(255,true, 4);
-  rijd(255,false,1);
- */ 
+
+void schrijfx() {
+//eerst draai
+  draai(1, rechts); // 1
+  
+  // Naar voren rijden
+  rijd(2.5);
+  // Dan naar achter
+  rijd(1.25, achteruit);
+  draai(2, rechts);
+  
+  rijd(1.25,achteruit);
+  
+  rijd(2.5,vooruit);
+  
+  draai(.75, rechts);
+
 }
 
 // Rijd voor of achteruit voor een bepaalde tijd in seconden
 void rijd(float duur, boolean richting, byte power) {
-    motor(1, power, richting);
-    if(richting) {
-        motor(2,power-40,richting);
-    } else{
-        motor(2,power-40,richting);  
-    }
+    power = 255;
+    motor(motor_rechts, power, richting);
+     motor(motor_links,power,richting);
+//    if(richting) {
+//        motor(motor_links,power-40,richting);
+//    } else{
+//        motor(motor_links,power-45,richting);  
+//    }
     //motor(2,power,richting);
     delay(duur*1000);
     stastil();
@@ -139,28 +321,68 @@ void stastil() {
 }
 
 // Draai de robot voor een bepaalde tijd
-// 1 seconde = 60 graden draai
-// 3 seconden = 180 graden
-void draai(int duur, boolean richting, byte power) {
-  if(power > 215) power = 215
-  motor(1,power,richting);
-  motor(2,power+40,!richting);
+void draai(float duur, boolean richting, byte power) {
+  motor(1, power, richting);
+  motor(2, power,!richting);
   delay(duur*1000);
   stastil();
 }
 
-void draai(int duur, boolean richting) {
-  byte power = 200;
+void draai(float duur, boolean richting) {
+  byte power = 255;
   draai(duur, richting, power);
 }
 
-void toonnummer(int nummer) {
-  toonL();
+void draaiGraden(float graden, boolean richting) {
+
+  byte power = 255;
+  motor(1, power, richting);
+  motor(2, power,!richting);
+  
+  delay(graden*18);
+  
+  stastil();
+}
+
+boolean checkbutton(){
+ int buttonstate = analogRead(A3);
+ if(buttonstate > 1020) {
+  return true;
+ } else {
+  return false; 
+ }
+}
+void schrijfl(){
+    digitalWrite(1, LOW);
+    digitalWrite(13, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(12, LOW);
+    
+    digitalWrite(3, LOW); // GROUND
+    digitalWrite(2, HIGH); // GROUND
+    
+    
+}
+void dimmen(){
+    digitalWrite(1, LOW);
+    digitalWrite(13, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(12, LOW);
+
+}
+void schrijfcijfer(int cijfer){
+  schrijfl();
   delay(5);
   
   digitalWrite(3, HIGH); // GROUND
   digitalWrite(2, LOW); // GROUND
-  switch (nummer){
+  switch (cijfer){
     case 0:
       digitalWrite(1, LOW);
       digitalWrite(13, HIGH);
@@ -256,36 +478,3 @@ void toonnummer(int nummer) {
   
   delay(5);
 }
-
-void toonL() {
-    digitalWrite(1, LOW);
-    digitalWrite(13, HIGH);
-    digitalWrite(11, HIGH);
-    digitalWrite(10, HIGH);
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(12, LOW);
-    
-    digitalWrite(3, LOW); // GROUND
-    digitalWrite(2, HIGH); // GROUND
-}
-
-void toonniks() {
-  digitalWrite(1, LOW);
-    digitalWrite(13, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(12, LOW);
-}
-
-boolean checkbutton(){
- int buttonstate = analogRead(A3);
- if(buttonstate > 1020) {
-  return true;
- } else {
-  return false; 
- }
-}
-
